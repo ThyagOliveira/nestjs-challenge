7ab306522351737f9475595cc784f75b99e54b4a
ThyagOliveira <taothyago@gmail.com>
2023-05-22 00:36:08 +0100

    :gem: Add users CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Consort Group')
+    .setDescription('API')
+    .setVersion('1.0')
+    .addTag('API TAG')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Users')
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @ApiCreatedResponse({ type: UserEntity })
+  async create(@Body() createUserDto: CreateUserDto) {
+    return new UserEntity(await this.usersService.create(createUserDto));
+  }
+
+  @Get()
+  @ApiOkResponse({ type: UserEntity, isArray: true })
+  async findAll() {
+    const users = await this.usersService.findAll();
+
+    return users.map((user) => new UserEntity(user));
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: UserEntity })
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findOne(+id);
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} does not exist`);
+    }
+
+    return new UserEntity(user);
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ type: UserEntity })
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const user = await this.usersService.findOne(+id);
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} does not exist`);
+    }
+
+    return new UserEntity(await this.usersService.update(+id, updateUserDto));
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: UserEntity })
+  async remove(@Param('id') id: string) {
+    const user = await this.usersService.findOne(+id);
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} does not exist`);
+    }
+
+    return new UserEntity(await this.usersService.remove(+id));
+  }
+}
